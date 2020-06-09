100cdfce59caf833dcda5253ea98e3ba4ab52ca4
deep shikha <deepshikhasingh90@outlook.com>
2020-06-09 13:35:12 -0700

    initial version

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Results from "../components/Results"
+
+function NoMatch() {
+  return (
+    <div>
+         <Results>
+         <h1>404 Page Not Found</h1>
+         </Results>
+            
+          
+    </div>
+  );
+}
+
+export default NoMatch;
